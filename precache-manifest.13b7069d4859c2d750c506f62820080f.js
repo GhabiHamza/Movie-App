2ba164a97c328cd82838350d7771dafa4cc239e7
GhabiHamza <ghabihamza77@gmail.com>
2019-08-28 17:29:09 +0200

    Updates

---
+++ precache-manifest.13b7069d4859c2d750c506f62820080f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6feaa46d23d919685396a6e390ed285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5080c135e6756fb5aa76",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4b1164f42db58fc4225c",
+    "url": "/static/css/main.767a3af0.chunk.css"
+  },
+  {
+    "revision": "5080c135e6756fb5aa76",
+    "url": "/static/js/2.471d9ebd.chunk.js"
+  },
+  {
+    "revision": "4b1164f42db58fc4225c",
+    "url": "/static/js/main.7662c3ef.chunk.js"
+  },
+  {
+    "revision": "d4cbcd78524d4aeba4cd",
+    "url": "/static/js/runtime~main.23a7e1f0.js"
+  },
+  {
+    "revision": "8b99e73602bd5c533ef99cbbaa172f1c",
+    "url": "/static/media/ASingleShotPoster.8b99e736.png"
+  },
+  {
+    "revision": "f7a55362b59a9469a6bd79b90959eb4f",
+    "url": "/static/media/A_Simple_Favor.f7a55362.png"
+  },
+  {
+    "revision": "6c9be206062c4a8e2408b6f69b90a71c",
+    "url": "/static/media/The_Spy_Who_Dumped_Me.6c9be206.png"
+  },
+  {
+    "revision": "a6ff1a004126982ed8637b216d89cf65",
+    "url": "/static/media/inception.a6ff1a00.jpg"
+  },
+  {
+    "revision": "ae718ee98848314896669e92d2730f39",
+    "url": "/static/media/showshank.ae718ee9.jpg"
+  }
+]);
